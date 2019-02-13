00ea782743a5587b3663ac7f30912e8e41c6af22
Christopher Martin <martincdev@gmail.com>
2019-02-13 04:46:46 -0600

    put useResources into its own file, add UserList to show hook reusability

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
   /*
@@ -25,6 +26,7 @@ const App = () => {
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource('posts')}>
           Posts
