9c617552b43e16cb5e35a03aed2ab4c132c48175
Christopher Martin <martincdev@gmail.com>
2019-02-13 03:23:48 -0600

    add ResourceList, pass props to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   /*
@@ -31,7 +32,7 @@ const App = () => {
           Todos
         </button>
       </div>
-      {resource}
+      <ResourceList resource={resource}/>
     </div>
   )
 };
